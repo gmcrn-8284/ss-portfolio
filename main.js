cf022256642fe59ff58d5aca1b19895c8df91397
Kohei Manabe <manaemon0629@icloud.com>
2022-11-19 06:06:01 +0900

    変更3

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 function alertButton() {
     alert('クリックされました')
 }
+
+let button = document.querySelector('.side-menu-btn-open');
+let openMenu = function() {
+    button.classList.add('active');
+}

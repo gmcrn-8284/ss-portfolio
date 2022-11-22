c72011e5244cf3143d79eb58a42c74758380825c
Kohei Manabe <manaemon0629@icloud.com>
2022-11-22 20:24:04 +0900

    カウントアップ関数の変更１

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+let counter, addBtn;
+let n;
+
+function addCount() {
+    n ++;
+    counter.innerHTML = n;
+}
+window.addEventListener("load", ()=> {
+    counter = document.getElementById("counter");
+    addBtn = document.getElementById("addBtn");
+    n = 0;
+
+    addBtn.addEventListener("click", addCount)
+});
+
+
+
+
+
 
 
 // <--------------   サイドメニューのopen, close関数     -------------------->
@@ -36,11 +55,6 @@ let havePikmin = [0, 1]
 let result = []
 function restaurantChangeImg(i, c) {
     
-    // if (havePikmin[i] == 1) {
-    //     havePikmin[i] = 0;
-    // } else {
-    //     havePikmin[i] ++;
-    // }
     const pikminImg = document.getElementsByClassName('restaurantPikminImg');
     if (pikminImg[c].getAttribute('src') == restaurant_pic_srcs[c][0]) {
         pikminImg[c].src = restaurant_pic_srcs[c][1];
@@ -48,19 +62,7 @@ function restaurantChangeImg(i, c) {
         pikminImg[c].src = restaurant_pic_srcs[c][0];
     }
 
-    // pikminImg[c].src=restaurant_pic_srcs[c][havePikmin[i]];
-    // console.log(pikminImg[c].src);
-    // console.log(havePikmin.reduce(function(sum, element){return sum+element}, 0))
-    // if(havePikmin[i] == 1) {
-    //     result.push(1);
-    // }else {
-    //     result.shift();
-    // }
-    // console.log(result)
-    // console.log(result.length)
 }
-// const count = result.length;
-// console.log(count)
 
 
 // <--------------   カフェタイプの画像配列と切り替え処理の関数     -------------------->
@@ -74,15 +76,14 @@ let cafe_pic_srcs = new Array(
     ["image/not-have-pikmin.jpg","image/have-pikmins/cafe-wing.jpg"]
 );
 function cafeChangeImg(i, c) {
-    if (havePikmin[i] == 1) {
-        havePikmin[i] = 0;
-    } else {
-        havePikmin[i] ++;
-    }
+    
     const pikminImg = document.getElementsByClassName('cafePikminImg');
-    pikminImg[c].src=cafe_pic_srcs[c][havePikmin[i]];
-    console.log(havePikmin[i]);
-    // console.log(havePikmin.reduce(function(sum, element){return sum+element}, 0))
+    if (pikminImg[c].getAttribute('src') == cafe_pic_srcs[c][0]) {
+        pikminImg[c].src = cafe_pic_srcs[c][1];
+    } else if (pikminImg[c].getAttribute('src') == cafe_pic_srcs[c][1]) {
+        pikminImg[c].src = cafe_pic_srcs[c][0];
+    }
+
 }
 
 // <--------------   薬局タイプの画像配列と切り替え処理     -------------------->
@@ -96,15 +97,14 @@ let pharmacy_pic_srcs = new Array(
     ["image/not-have-pikmin.jpg","image/have-pikmins/pharmacy-wing.jpg"]
 );
 function pharmacyChangeImg(i, c) {
-    if (havePikmin[i] == 1) {
-        havePikmin[i] = 0;
-    } else {
-        havePikmin[i] ++;
-    }
+    
     const pikminImg = document.getElementsByClassName('pharmacyPikminImg');
-    pikminImg[c].src=pharmacy_pic_srcs[c][havePikmin[i]];
-    console.log(havePikmin[i]);
-    // console.log(havePikmin.reduce(function(sum, element){return sum+element}, 0))
+    if (pikminImg[c].getAttribute('src') == pharmacy_pic_srcs[c][0]) {
+        pikminImg[c].src = pharmacy_pic_srcs[c][1];
+    } else if (pikminImg[c].getAttribute('src') == pharmacy_pic_srcs[c][1]) {
+        pikminImg[c].src = pharmacy_pic_srcs[c][0];
+    }
+
 }
 
 // <--------------   水辺タイプの画像配列と切り替え処理     -------------------->
@@ -118,15 +118,14 @@ let river_pic_srcs = new Array(
     ["image/not-have-pikmin.jpg","image/have-pikmins/river-wing.jpg"]
 );
 function riverChangeImg(i, c) {
-    if (havePikmin[i] == 1) {
-        havePikmin[i] = 0;
-    } else {
-        havePikmin[i] ++;
-    }
+    
     const pikminImg = document.getElementsByClassName('riverPikminImg');
-    pikminImg[c].src=river_pic_srcs[c][havePikmin[i]];
-    console.log(havePikmin[i]);
-    // console.log(havePikmin.reduce(function(sum, element){return sum+element}, 0))
+    if (pikminImg[c].getAttribute('src') == river_pic_srcs[c][0]) {
+        pikminImg[c].src = river_pic_srcs[c][1];
+    } else if (pikminImg[c].getAttribute('src') == river_pic_srcs[c][1]) {
+        pikminImg[c].src = river_pic_srcs[c][0];
+    }
+
 }
 
 // <--------------   公園タイプの画像配列と切り替え処理     -------------------->
@@ -140,14 +139,13 @@ let park_pic_srcs = new Array(
     ["image/not-have-pikmin.jpg","image/have-pikmins/park-wing.jpg"]
 );
 function parkChangeImg(i, c) {
-    if (havePikmin[i] == 1) {
-        havePikmin[i] = 0;
-    } else {
-        havePikmin[i] ++;
-    }
+    
     const pikminImg = document.getElementsByClassName('parkPikminImg');
-    pikminImg[c].src=park_pic_srcs[c][havePikmin[i]];
-    console.log(havePikmin[i]);
-    // console.log(havePikmin.reduce(function(sum, element){return sum+element}, 0))
+    if (pikminImg[c].getAttribute('src') == park_pic_srcs[c][0]) {
+        pikminImg[c].src = park_pic_srcs[c][1];
+    } else if (pikminImg[c].getAttribute('src') == park_pic_srcs[c][1]) {
+        pikminImg[c].src = park_pic_srcs[c][0];
+    }
+
 }
 
